c3ccf45114f1f90afdd9b692e105a28de58f508b
Tech_cooker <tsotneberishvili@gmail.com>
2026-06-06 19:17:58 +0400

    fixed syntax

--- c3-datasources-locals.tf
+++ c3-datasources-locals.tf
@@ -3,7 +3,7 @@ data "aws_availability_zone" "avaiable" {
 }
 
 locals {
-  azs=slice(data.aws_availability_zone.avaiable.name,0,2)
+  azs=slice(data.aws_availability_zone.avaiable.names,0,2)
   public_subnet=[for k, az in local.azs : cidrsubnet(var.cidr,var.newbits,k)]
   private_subnet=[for k, az in local.azs : cidrsubnet(var.cidr,var.newbits,k+10)]
 }
