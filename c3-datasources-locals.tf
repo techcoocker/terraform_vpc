890fb2566fa129546b0d9652dcaf4bece5977658
Tech_cooker <tsotneberishvili@gmail.com>
2026-06-06 19:23:48 +0400

    Hope final fix

--- c3-datasources-locals.tf
+++ c3-datasources-locals.tf
@@ -1,9 +1,9 @@
-data "aws_availability_zone" "avaiable" {
+data "aws_availability_zones" "avaiable" {
   state = "available"
 }
 
 locals {
-  azs=slice(data.aws_availability_zone.avaiable.names,0,2)
+  azs=slice(data.aws_availability_zones.avaiable.names,0,2)
   public_subnet=[for k, az in local.azs : cidrsubnet(var.cidr,var.newbits,k)]
   private_subnet=[for k, az in local.azs : cidrsubnet(var.cidr,var.newbits,k+10)]
 }
